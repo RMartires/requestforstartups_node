ab3612b23d3a3bfd7a5fb4ef038c3da00a1fd73f
RMartires <rohitmartires14@gmail.com>
2020-02-16 22:30:52 +0530

    fixed typo

--- controllers/twitter.js
+++ controllers/twitter.js
@@ -3,7 +3,7 @@ var base = require('../database/airtable');
 
 exports.gettweets = (req, res, next) => {
     var client = new Twitter({
-        consumer_key: process.env.consumer_secret,
+        consumer_key: process.env.consumer_key,
         consumer_secret: process.env.consumer_secret,
         access_token_key: process.env.access_token_key,
         access_token_secret: process.env.access_token_secret
