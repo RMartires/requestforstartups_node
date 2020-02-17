471677fc4e794c1dbaea71ca0f450b2bda883ac7
RMartires <rohitmartires14@gmail.com>
2020-02-17 22:44:16 +0530

    added server side validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,9 @@ const bcrypt = require('bcryptjs');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 const cookie = require('js-cookie');
-//airtable
+const { validationResult } = require('express-validator');
 
+//airtable
 var base = require('../database/airtable');
 
 const mainurl = require('../database/links');
@@ -14,70 +15,81 @@ exports.postSignup = (req, res, next) => {
     const password = req.body.password;
     const confirmpassword = req.body.confirmpassword;
     const username = req.body.name;
+    const valerrors = validationResult(req);
 
     var topuserid;
     //console.log(password + ' ' + confirmpassword);
 
-    if (password === confirmpassword) {
-        var exist = false;
-        base('users').select({
-            fields: ["Email"],
-            cellFormat: "json",
-            view: "Grid view"
-        })
-            .eachPage((records) => {
-                records.map(record => {
-                    var { fields } = record;
-                    var { Email } = fields;
-                    if (Email === email) {
-                        exist = true;
-                    }
-                });
-                if (exist) {
-                    //console.log('user alredy exists');
-                    res.json({
-                        message: 'email exists'
-                    });
-                } else {
-                    bcrypt.hash(password, 12)
-                        .then(hashedpassword => {
-                            base('users').create([{
-                                "fields": {
-                                    "Name": username,
-                                    "Email": email,
-                                    "Password": hashedpassword,
-                                    "ideas": [],
-                                    "comments": []
-                                }
-                            }], (err, records) => {
-                                if (records) {
-                                    var { id } = records[0];
-                                    topuserid = id;
-                                    console.log(topuserid);
-                                    const token = jwt.sign({
-                                        email: email,
-                                        loggedin: true,
-                                        userid: topuserid
-                                    }, 'heyphil123');
-                                    res.json({
-                                        message: 'done',
-                                        token: token
-                                    });
-                                } else {
-                                    console.log(err);
-                                }
-                            })
-                        });
-                }
-            }, err => {
-                console.log(err);
-            });
-    } else {
-        //console.log("password does not match");
+    if (!valerrors.isEmpty()) {
         res.json({
-            message: 'password did not match'
+            message: valerrors.array()[0].msg
         });
+    } else {
+        //in feilds are valid
+        if (password === confirmpassword) {
+            var exist = false;
+            base('users').select({
+                fields: ["Email"],
+                cellFormat: "json",
+                view: "Grid view"
+            })
+                .eachPage((records) => {
+                    records.map(record => {
+                        var { fields } = record;
+                        var { Email } = fields;
+                        if (Email === email) {
+                            exist = true;
+                        }
+                    });
+                    if (exist) {
+                        //console.log('user alredy exists');
+                        res.json({
+                            message: 'User with this email exists'
+                        });
+                    } else {
+                        bcrypt.hash(password, 12)
+                            .then(hashedpassword => {
+                                base('users').create([{
+                                    "fields": {
+                                        "Name": username,
+                                        "Email": email,
+                                        "Password": hashedpassword,
+                                        "ideas": [],
+                                        "comments": []
+                                    }
+                                }], (err, records) => {
+                                    if (records) {
+                                        var { id } = records[0];
+                                        topuserid = id;
+                                        console.log(topuserid);
+                                        const token = jwt.sign({
+                                            email: email,
+                                            loggedin: true,
+                                            userid: topuserid
+                                        }, 'heyphil123');
+                                        res.json({
+                                            message: 'done',
+                                            token: token
+                                        });
+                                    } else {
+                                        console.log(err);
+                                    }
+                                })
+                            });
+                    }
+                }, err => {
+                    console.log(err);
+                });
+        } else {
+            //console.log("password does not match");
+            res.json({
+                message: 'Password and confirmpassword feild should match'
+            });
+        }
+
+        //end after validdation part
     }
+
 
 };
 
@@ -87,54 +99,62 @@ exports.postLogin = (req, res, next) => {
     var exist = false;
     var topPassword;
     var userid;
-
-    base('users').select({
-        fields: ["Email", "Password"],
-        cellFormat: "json",
-        view: "Grid view"
-    })
-        .eachPage((records) => {
-            records.map(record => {
-                var { fields } = record;
-                var { Email } = fields;
-                var { Password } = fields;
-                if (Email === email) {
-                    exist = true;
-                    topPassword = Password;
-                    userid = record.id;
-                    console.log(Password);
-                }
-            });
-            if (exist) {
-                bcrypt.compare(password, topPassword)
-                    .then(doMatch => {
-                        console.log(doMatch);
-                        if (exist && doMatch) {
-                            console.log('loggedin');
-                            const token = jwt.sign({
-                                email: email,
-                                loggedin: true,
-                                userid: userid
-                            }, 'heyphil123');
-                            console.log(token);
-                            res.json({
-                                message: 'done',
-                                token: token
-                            });
-
-                        } else {
-                            res.json({
-                                message: 'wrong password'
-                            });
-                        }
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-            }
-
-        }, err => {
-            console.log(err);
+    const valerrors = validationResult(req);
+    if (!valerrors.isEmpty()) {
+        res.json({
+            message: valerrors.array()[0].msg
         });
+    } else {
+        //if inputs are valid
+        base('users').select({
+            fields: ["Email", "Password"],
+            cellFormat: "json",
+            view: "Grid view"
+        })
+            .eachPage((records) => {
+                records.map(record => {
+                    var { fields } = record;
+                    var { Email } = fields;
+                    var { Password } = fields;
+                    if (Email === email) {
+                        exist = true;
+                        topPassword = Password;
+                        userid = record.id;
+                        console.log(Password);
+                    }
+                });
+                if (exist) {
+                    bcrypt.compare(password, topPassword)
+                        .then(doMatch => {
+                            console.log(doMatch);
+                            if (exist && doMatch) {
+                                console.log('loggedin');
+                                const token = jwt.sign({
+                                    email: email,
+                                    loggedin: true,
+                                    userid: userid
+                                }, 'heyphil123');
+                                console.log(token);
+                                res.json({
+                                    message: 'done',
+                                    token: token
+                                });
+
+                            } else {
+                                res.json({
+                                    message: 'Email and password did not match'
+                                });
+                            }
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
+                }
+
+            }, err => {
+                console.log(err);
+            });
+
+    }
 
 };
--- controllers/twitter.js
+++ controllers/twitter.js
@@ -2,19 +2,20 @@ const Twitter = require('twitter');
 var base = require('../database/airtable');
 
 exports.gettweets = (req, res, next) => {
+    console.log('listening for tweets');
     var client = new Twitter({
         consumer_key: '2UEKwIijR55ZTyG7t6ccxfCVn',
         consumer_secret: 'kzTOcsI4KRSfQTeaCwYFPsccXcRoU0xNmN33cqYgwv3j7KV9an',
-        access_token_key: '1228590283207499776-N2sUQ1zsiZ4HM4lF1Mc2UKSKQbZVGf',
-        access_token_secret: 'bgjlS97XXIWM6yiWHzlDHf7R8hWHqlSAOOsSgt6ZPFqkE'
+        access_token_key: '1228590283207499776-CMWZjHRQ83nS6EjoOlBmrsQzo3dwN9',
+        access_token_secret: '9xjAqcFpAds672m585jGmRQ1ADltKs5b43GjS57228za5'
     });
 
     var stream = client.stream('statuses/filter', { track: '@startuprequest' });
     stream.on('data', function (event, error) {
+        console.log('d');
         if (error) {
             console.log(errror);
         }
-        console.log('listening for tweets');
         console.log(event.text);
 
         var { id } = event;
