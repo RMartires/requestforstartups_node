4a3fc4ac5dbd1fccdf0fca07deebfc52fd531680
RMartires <rohitmartires14@gmail.com>
2020-03-20 01:13:29 +0530

    made link changes

--- database/links.js
+++ database/links.js
@@ -1,4 +1,5 @@
 //const mainurl = 'http://localhost:3000'
-const mainurl = 'https://cryptic-sea-72911.herokuapp.com';
+//const mainurl = 'https://cryptic-sea-72911.herokuapp.com';
+const mainurl = 'https://www.requestforstartups.in';
 
 module.exports = mainurl;
